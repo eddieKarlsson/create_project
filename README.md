70184c51844a0d63861a4571ae4dac936a825c81
eddieKarlsson <eddieperty@gmail.com>
2022-05-30 13:02:42 +0200

    openpyxl appempt

---
+++ README.md
@@ -0,0 +1,11 @@
+# Create Project
+## Creates project tree and renames files from template, edits predefined cells in excel workbooks.
+
+Requirements:
+1. pip install openpyxl
+2. Template project
+
+Commandline argument:
+1. New project name
+2. New project path
+3. Project number
--- create_project_free.py
+++ create_project_free.py
@@ -3,12 +3,43 @@ import shutil
 import sys
 import openpyxl as xl
 
+DOC_PATH = r'C:\Users\EddieKarlsson\Documents'
+TEMPLATE_PATH = r'project_structure\free'
+
 new_proj_name = sys.argv[1]
 dest_path = os.path.join(sys.argv[2], sys.argv[1])
 projnr = sys.argv[3]
 
-DOC_PATH = r'C:\Users\EddieKarlsson\Documents'
-TEMPLATE_PATH = r'project_structure\free'
+def _openpyxl_open_workbook_and_edit_attr(excel_path):
+    try:
+        wb = xl.load_workbook(excel_path)
+    except FileNotFoundError as e:
+        print(e)
+        printf('ERROR! Excel {excel_path} file not found, program will exit')
+        sys.exit()
+
+    ws = wb['Overview']
+    #ws = wb.active
+
+    # Shift index right in sheet until "Projectnumber" text is found
+    for i in range(2, 99):
+        cell = ws.cell(row=1, column=i)
+
+        if cell.value and 'number' in cell.value:
+            print(cell)
+            col_index = i
+            break
+        else:
+            continue
+
+        # Write to cells
+        ws.cell(row=1, column=col_index).value = projnr
+        ws.cell(row=2, column=col_index).value = new_proj_name
+
+    wb.save(excel_path)
+
+
+
 source_path = os.path.join(DOC_PATH, TEMPLATE_PATH)
 
 shutil.copytree(source_path, dest_path)
@@ -21,4 +52,4 @@ td_src_path = os.path.join(DOC_PATH, 'MC Doc\TD', 'latest_td.xlsx')
 td_dst_path = os.path.join(dest_path, 'doc', new_proj_name + '_td.xlsx')
 shutil.copy(td_src_path, td_dst_path)
 
-#TODO - open excels with openpyxl and edit projnr and name
+_open_workbook_and_edit_attr(td_dst_path)
